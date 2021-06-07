7760ebcfabac5d089fff1e696f3b840a50587915
jimmycababa <jimmycababa@gmail.com>
2021-06-07 15:51:38 -0500

    added all folders and packages

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const session = require(express-session)
+const MongoStore = require(connect-mongo)(session)
+const connectDB = require(',/config/database')
+require('dotenv').config({path: './config/.env'})
