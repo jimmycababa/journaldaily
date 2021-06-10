df86cd77fc6f73b9493bc7c13e5b045ae73a83d1
jimmycababa <jimmycababa@gmail.com>
2021-06-10 06:41:38 -0500

    added db stuff

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const session = require(express-session)
-const MongoStore = require(connect-mongo)(session)
-const connectDB = require(',/config/database')
-const mainRoutes = require('./routes/main')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const connectDB = require('./config/database')
+// const mainRoutes = require('./routes/main')
+// const journalRoutes = require('./routes/journal')
 require('dotenv').config({path: './config/.env'})
+
+connectDB()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 // what does this line do?
-app.use(epress.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
+// app.use(logger('dev'))
 
 // sessions
 app.use(
@@ -23,9 +28,10 @@ app.use(
     })
 )
 
-app.use('/', mainRoutes)
-app.use('/journal', journalRoutes)
-app.use('/feed', feedRoutes)
+
+// app.use('/', mainRoutes)
+// app.use('/journal', journalRoutes)
+// app.use('/feed', feedRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log('Serving running, but is it fast?')
